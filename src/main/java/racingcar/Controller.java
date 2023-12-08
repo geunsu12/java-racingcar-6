8240a80a27e80883123fbb0a2a1734d452a8dabc
geunsu <geunsu23@naver.com>
2023-12-08 15:21:25 +0900

    feat: Controller 추가

---
+++ src/main/java/racingcar/Controller.java
@@ -0,0 +1,42 @@
+package racingcar;
+
+import racingcar.model.CarName;
+import racingcar.model.CarPosition;
+import racingcar.model.RandomMove;
+import racingcar.model.Repetition;
+import racingcar.view.InputView;
+import racingcar.view.Outputview;
+
+public class Controller {
+
+    private CarName carName;
+    private CarPosition carPosition;
+    private Repetition repetition;
+    private int carNumber;
+
+    private Controller() {}
+
+    public static Controller createController() {
+        return new Controller();
+    }
+
+    public void prepareEngine() {
+        Outputview.printRequireCarNameMessage();
+        this.carName = CarName.createCarName(InputView.readLine());
+        this.carNumber = carName.getCarNumber();
+        this.carPosition = CarPosition.createCarPosition(carNumber);
+
+        Outputview.printRequireTimesMessage();
+        this.repetition = Repetition.createRepetition(InputView.readLine());
+    }
+
+    public void runEngine() {
+        while (repetition.repeatAvailable().equals(true)) {
+            RandomMove randomMove = RandomMove.createRandomMove(carNumber);
+            carPosition.moveCarPosition(randomMove.getRandomMove());
+            repetition.increaseRepetition();
+        }
+        Outputview.printRaceResult(carName, carPosition);
+    }
+
+}
