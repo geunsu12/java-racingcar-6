0c98f5a80f40adef4ad1a855f040610d2b929614
geunsu <geunsu23@naver.com>
2023-12-08 15:22:11 +0900

    feat: 프로그램 상수 관리 코드 추가

---
+++ src/main/java/racingcar/constants/Constants.java
@@ -0,0 +1,14 @@
+package racingcar.constants;
+
+public class Constants {
+    private Constants() {}
+
+    public static final int MAX_NAME_LENGTH = 5;
+
+    public static final int MIN_RANDOM_NUM = 0;
+    public static final int MAX_RANDOM_NUM = 9;
+    public static final int MOVE_THRESHOLD = 4;
+    public static final int MOVE = 1;
+    public static final int NON_MOVE = 0;
+
+}
