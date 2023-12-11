7831493bf606fb89e8d7073ca295f8da63a213e6
geunsu <geunsu23@naver.com>
2023-12-11 10:10:46 +0900

    feat: 우승자 출력 추가

--- src/main/java/racingcar/view/Outputview.java
+++ src/main/java/racingcar/view/Outputview.java
@@ -16,8 +16,16 @@ public class Outputview {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
+    public static void printResultMessage() {
+        System.out.println("실행 결과");
+    }
+
     public static void printRaceResult(CarName carName, CarPosition carPosition) {
         System.out.println(OutputService.createResult(carName, carPosition));
+    }
+
+    public static void printRaceWinner(CarName carName, CarPosition carPosition) {
+        System.out.println(OutputService.createWinnerResult(carName, carPosition));
     }
 
 }
