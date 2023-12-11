04c2411374b295e1ed83acec3d5984608fdaa5ea
geunsu <geunsu23@naver.com>
2023-12-11 10:11:12 +0900

    feat: 우승자 출력 위한 서비스 로직 추가

--- src/main/java/racingcar/service/OutputService.java
+++ src/main/java/racingcar/service/OutputService.java
@@ -3,6 +3,8 @@ package racingcar.service;
 import racingcar.model.CarName;
 import racingcar.model.CarPosition;
 
+import java.util.List;
+
 public class OutputService {
     private OutputService() {}
 
@@ -17,5 +19,16 @@ public class OutputService {
         }
         return sb.toString();
     }
+
+    public static String createWinnerResult(CarName carName, CarPosition carPosition) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("최종 우승자 : ");
+        List<String> winners = carName.getWinnersName(carPosition.getRaceWinner());
+
+        sb.append(String.join(",",winners));
+
+        return sb.toString();
+    }
+
 
 }
